79358dada7c7ab225c2fc5bc98a5926652d065ea
Clément Aguilar <aguilar.clement@outlook.fr>
2021-06-01 11:36:26 +0200

    Increase block retry interval

--- chains/ethereum/listener.go
+++ chains/ethereum/listener.go
@@ -25,7 +25,7 @@ import (
 	ethcommon "github.com/ethereum/go-ethereum/common"
 )
 
-var BlockRetryInterval = time.Second * 5
+var BlockRetryInterval = time.Second * 20
 var BlockRetryLimit = 5
 var ErrFatalPolling = errors.New("listener block polling failed")
 
